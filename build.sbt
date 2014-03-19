50befce1acb0e407c98e76d28be126c9580e180b
ayubmalik <ayub.malik@gmail.com>
2014-03-19 21:39:44 +0000

    Initial

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "akka-book"
+
+version := "1.0"
+
+scalaVersion := "2.10.2"
+
+libraryDependencies ++= Seq(
+  "org.scalatest" % "scalatest_2.10" % "2.0",
+  "junit" % "junit" % "4.11",
+  "com.typesafe.akka" %% "akka-actor" % "2.2.1"
+)
---
+++ src/main/scala/ayub/MyActor.scala
@@ -0,0 +1,30 @@
+package ayub
+
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+import akka.actor.ActorLogging
+
+case class Message(msg: String)
+
+class MyActor extends Actor with ActorLogging{
+
+  def receive = {
+    case Message(m) => log.info(s"got message {}", m)
+  }
+}
+
+object MyActorMain  {
+  val system = ActorSystem("myActor")
+  
+  val actor = system.actorOf(Props[MyActor], "myActor")
+  
+  def main(args: Array[String]) {
+    actor ! "hello"
+    actor ! Message ("poo")
+    actor ! Message ("hoo")
+    actor ! Message ("boo")
+    system.shutdown
+  }
+  
+}
